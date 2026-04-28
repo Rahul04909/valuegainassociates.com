3559c1d60dc042e8b896678ba05dd266508625d4
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-04-28 18:28:09 +0530

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -54,7 +54,7 @@
                     <li><a href="about.php">About Us</a></li>
                     <li><a href="properties.php">Properties</a></li>
                     <li><a href="contact.php">Contact Us</a></li>
-                    <li><a href="#">Our Agents</a></li>
+                    <!-- <li><a href="#">Our Agents</a></li> -->
                 </ul>
             </div>
 
